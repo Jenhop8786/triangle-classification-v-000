5437873e32373ad2fd27bff4493b579e0838787f
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-17 23:51:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,50 +1,50 @@
 class Triangle
 
-attr_accessor :a, :b, :case 
-attr_reader :type 
+attr_accessor :a, :b, :case
+attr_reader :type
 
 def initialize(a, b, c)
   @a = a
-  @b = b 
-  @c = c 
-end 
+  @b = b
+  @c = c
+end
 
 def equilateral?
-  self.a == self.b && self.b == self.c 
-end 
+  self.a == self.b && self.b == self.c
+end
 
 def isosceles?
-  self.a == self.b || self.b == self.c || self.a == self.c 
-end 
+  self.a == self.b || self.b == self.c || self.a == self.c
+end
 
 def scalene?
-  self.a != self.b && self.b != self.c && self.a != self.c 
-end 
+  self.a != self.b && self.b != self.c && self.a != self.c
+end
 
 def illegal?
-  if self.a <= 0 || self.b <= 0 || self.c <= 0 || self.a + self.b <= self.c || self.b + self.c <= self.a || self.a + self.c <= self.b 
-    true 
-  end 
-end 
+  if self.a <= 0 || self.b <= 0 || self.c <= 0 || self.a + self.b <= self.c || self.b + self.c <= self.a || self.a + self.c <= self.b
+    true
+  end
+end
 
-def kind 
+def kind
   if illegal?
     raise TriangleError
     puts error.message
-  elsif 
+  elsif
     equilateral?
     @type = :equilateral
-  elsif 
-    isosceles? 
+  elsif
+    isosceles?
     @type = :scalene
-  end 
-end 
-end 
+  end
+end
+end
 
 class TriangleError < StandardError
-  def message 
-    "Please enter correct lengths."    
+  def message
+    "Please enter correct lengths."
   end
-    
+
   end
   
